4f314c6239c63d616a2739eeee228f2db4c2949b
cookiemiro <xodml9598@gmail.com>
2024-06-14 17:57:17 +0900

    클래스 명 수정

--- Operator/2024-06-14/Problem5.java
+++ Operator/2024-06-14/Problem5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class Main5 {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             String[] str1 = sc.nextLine().split(" ");
